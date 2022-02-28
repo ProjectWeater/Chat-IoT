df13f3dd106262ebac5119a6a5e3f4ef6f420318
ProjectWeater <89791784+ProjectWeater@users.noreply.github.com>
2022-02-28 21:37:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -31,16 +31,16 @@
 		$strRet = file_get_contents($url);
 		$strRet = json_decode($strRet);
 		$rain = $strRet->feeds[0]->field6;
-		// if ($rain >= 90.1){
-		// 	$rain == "ฝนตกหนักมาก";
-		// }else if ($rain >= 35.1){
-		// 	$rain == "ฝนตกหนัก";
-		// }else if ($rain >= 10.1){
-		// 	$rain == "ฝนตกปานกลาง";
-		// }else {
-		// 	$rain == "ฝนตกเล็กน้อย";
-		// }
-		$rep_msg['text'] = $rain;
+		if ($rain >= 90.1){
+			$lv_rain = "ฝนตกหนัก";
+		}else if ($rain >= 35.1){
+			$lv_rain = "ฝนตกหนัก";
+		}else if ($rain >= 10.1){
+			$lv_rain = "ฝนตกปาน";
+		}else {
+			$lv_rain = "ฝนตกเล็กน้อย";
+		};
+		$rep_msg['text'] = number_format($rain,0 "ลูกบาศลิตร");
 		$rep_msg['type']='text';
 	}else if($recv_msg == "คุณภาพอากาศ") {
 		$url = "https://api.thingspeak.com/channels/1555446/feeds.json?results=1";
