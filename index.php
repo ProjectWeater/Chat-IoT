3c880a07e701f54d23fcf51314b4a48880e0618e
ProjectWeater <89791784+ProjectWeater@users.noreply.github.com>
2022-02-28 21:52:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
 			$lv_rain == "ฝนตกปานกลาง";
 		}else {
 			$lv_rain == "ฝนตกเล็กน้อย";
-		}
+		}return $lv_rain;
 		$rep_msg['text'] = $lv_rain;
 		$rep_msg['type']='text';
 	}else if($recv_msg == "คุณภาพอากาศ") {
